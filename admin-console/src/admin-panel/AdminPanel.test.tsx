56f7e8ce63daf54c73fd60e4ea9484cefe906d29
kheva <khevamann@gmail.com>
2021-12-08 18:10:08 -0800

    remove all files

--- admin-console/src/admin-panel/AdminPanel.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import AdminPanel from './AdminPanel';
-
-test('renders learn react link', () => {
-  render(<AdminPanel />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- admin-console/src/agent-panel/AgentPanel.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import AgentPanel from './AgentPanel';
-
-test('renders learn react link', () => {
-  render(<AgentPanel />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
